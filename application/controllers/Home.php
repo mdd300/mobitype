3670ed9aa8e2baa9cb9fef805b2e27c91b4673e6
mdd300 <victor.za.oshiro5@gmail.com>
2018-08-13 17:09:01 -0300

    ajustes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,6 +18,12 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+    function __Construct()
+    {
+        parent::Controller();
+    }
+
 	public function index()
 	{
 	    $this->load->view('landing_page');
